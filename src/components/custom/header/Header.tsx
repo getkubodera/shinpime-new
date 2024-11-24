54992119f61bfe1910d214106ad155dcc004edb3
Adeeb Mohammed <adeeb.asharaf@tabist.com>
2024-11-24 10:38:47 +0900

    url change

--- src/components/custom/header/Header.tsx
+++ src/components/custom/header/Header.tsx
@@ -11,9 +11,9 @@ interface HeaderData {
 const headerData: HeaderData[] = [
     { name: "ホーム", href: "/" },
     { name: "ブログ", href: "https://blog.shinpi.me" },
-    { name: "製品", href: "/Products" },
+    { name: "製品", href: "/products" },
     { name: "概要", href: "/about" },
-    { name: "活動内容", href: "/work" },
+    /*{ name: "活動内容", href: "/work" },*/
     { name: "書籍", href: "/books" },
 ];
 
