e7fb30719240dcf0806a452ef027fafcaca63d8a
adeeb-asharaf <78344722+adeeb-asharaf@users.noreply.github.com>
2025-04-19 12:54:33 +0900

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "shinpi.me | 岳久保寺 | 人を深く励ます/神秘の私 | 内なる自由を見つける",
   description: "日本文化の根底にある空や無の世界観を現代に表現し、心と意識の自由を探求し人々の成長をサポートしながら、世界に新たな価値をもたらす。",
+  icons: {
+    icon: [
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', sizes: 'any' }
+    ],
+  },
 };
 
 export default function RootLayout({
